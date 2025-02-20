43813dcd23a44b1e71dffaab6e673ff29d8e62e7
FinleyCox <111890705+FinleyCox@users.noreply.github.com>
2025-02-20 22:27:39 +0900

    quiz-2-1

--- resources/js/components/views/Quiz.vue
+++ resources/js/components/views/Quiz.vue
@@ -4,36 +4,50 @@
         もう一度やるボタンでカウントリセットしてfetchquiz
     -->
     <div class="container">
-        <div v-if="questionCount < 5" class="quiz-container" id="quiz">
+        <!-- <div v-if="questionCount < 5" class="quiz-container" id="quiz"> -->
+        <div v-if="questionCount < questionTimes" class="quiz-container" id="quiz">
             <div class="quiz-header">
                 <h2>word quiz</h2>
-                <div class="progress">
-                    <div class="progress-bar" role="progressbar" style="width: 0%;" aria-valuenow="0" aria-valuemin="0"
-                        aria-valuemax="100"></div>
-                </div>
+                <!-- クイズ進行状況 ここから-->
+                    <div class="progress">
+                        <div class="progress-bar" role="progressbar">
+                            <p>
+                                <select v-model="questionTimes" class="form-select form-select-lg mb-3" aria-label=".form-select-lg">
+                                    <option value="5">5</option>
+                                    <option value="10">10</option>
+                                    <option value="15">15</option>
+                                    <option value="20">20</option>
+                                </select>
+                                出題数: {{ questionCount }} / {{ questionTimes }}
+                            </p>
+                        </div>
+                    </div>
+                <!-- クイズ進行状況 ここまで-->
             </div>
-            <div id="question-container">
-                <p class="question" id="question">
-                    {{ sicilian }}
-                </p>
-                <div class="options">
-                <button
-                    v-for="option in options" :key="option"
-                    :class="['option', 'btn', 'btn-outline-secondary', selectedOption === option ? (option === answer ? 'correct' : 'incorrect') : '']"
-                    @click="checkAnswer(option)
+            <!-- クイズ ここから -->
+                <div id="question-container">
+                    <p class="question" id="question">
+                        {{ sicilian }}
+                    </p>
+                    <div class="options">
+                        <button
+                            v-for="option in options" :key="option"
+                            :class="['option', 'btn', 'btn-outline-secondary', selectedOption === option ? (option === answer ? 'correct' : 'incorrect') : '']"
+                            @click="checkAnswer(option)
 
-                ">
-                    {{ option }}
-                </button>
-            </div>
-            </div>
-            <!-- <div class="quiz-footer">
-                <button class="btn btn-primary" id="next-btn">Next</button>
-            </div> -->
-            <p v-if="result !== null">{{ result }}</p>
+                        ">
+                            {{ option }}
+                        </button>
+                    </div>
+                </div>
+            <!-- クイズ ここまで -->
         </div>
+        <!-- 終了,結果、間違えた単語、もう一度 -->
         <div v-else>
             <h2>終了</h2>
+            <p v-if="result !== null">
+                {{ result }}
+            </p>
         </div>
     </div>
 </template>
@@ -49,7 +63,8 @@
                 answer: "",
                 result: null,
                 selectedOption: null,
-                questionCount: 0
+                questionCount: 0,
+                questionTimes: 5,
             };
         },
         mounted() {
@@ -62,7 +77,7 @@
                 // コンポーネントがマウントされたらAPIを呼ぶ
                 axios.get('/api/quiz')
                 .then(response => {
-                    console.log(response.data); // ここで JSON が返るか確認
+                    // console.log(response.data); // ここでJSONが返るか確認
                     this.sicilian = response.data.sicilian;
                     this.options = response.data.options;
                     this.answer = response.data.answer;
@@ -88,12 +103,15 @@
                 options.forEach(option => {
                     option.disabled = true;
                 });
-                this.nextQuestion();
+                if(this.questionCount < this.questionTimes) {
+                    this.nextQuestion();
+                }
+
                 options.forEach(option => {
                 option.disabled = false;
                 })
             },
-            // 3秒経ったら次の問題を呼ぶ(全部で５回まで)
+            // 3秒経ったら次の問題を呼ぶ
             nextQuestion() {
                 setTimeout(() => {
                     this.fetchQuiz();
