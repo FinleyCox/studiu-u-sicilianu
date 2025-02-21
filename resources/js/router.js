75ae81bcb8abc9a4849caa79bdc8dde4aff884cc
FinleyCox <111890705+FinleyCox@users.noreply.github.com>
2025-02-21 21:52:33 +0900

    fix design and add gif of demo

--- resources/js/router.js
+++ resources/js/router.js
@@ -3,7 +3,8 @@ import { createRouter, createWebHistory } from "vue-router";
 const routes = [
     // { path: '/', name: 'App', component: () => import('./components/App.vue') },
     { path: '/', name: 'Home', component: () => import('./components/views/Home.vue') },
-    { path: '/quiz', name: 'Quiz', component: () => import('./components/views/Quiz.vue') }
+    { path: '/quiz', name: 'Quiz', component: () => import('./components/views/Quiz.vue') },
+    { path: '/conjugation-essentials', name: 'Conjugation', component: () => import('./components/views/Conjugation.vue') },
 ]
 
 const router = createRouter({
